e40d75676e4235b62ebc6945c2b457e35ce24b61
jonathantsai <cat2006house@gmail.com>
2023-10-16 23:33:01 +0800

    Step 7

--- listings/urls.py
+++ listings/urls.py
@@ -3,6 +3,6 @@ from . import views
 # set the / api, as variable to callback function
 urlpatterns = [
     path('', views.index, name="listings"),
-    path('<int:listing_id>', views.listing, name="listing"),
+    path('listing', views.listing, name="listing"),
     path('search', views.search, name="search"),
 ]
